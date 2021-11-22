476afb702b4121d712565983414fd3c0cd3cbb25
alfredochs <chanta.alfredo@gmail.com>
2021-11-22 13:45:11 +0100

    layout header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <Welcome></Welcome>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "@/components/Header.vue";
+import Welcome from "@/components/Welcome.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Header, Welcome },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/scss/bootstrap";
+@import "@/styles/allStyles.scss";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="top d-flex justify-content-between align-items-center">
+      <div class="">Avada Health</div>
+
+      <div>
+        <ul class="d-flex list-unstyled align-items-center">
+          <li>Home</li>
+          <li>About</li>
+          <li>Departments</li>
+          <li>Articles</li>
+          <li>
+            <button class="btn btn-primary">Make Appointment</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="center-container">
+      <h2>Caring For Life</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis
+        nulla atque omnis laudantium repellendus veniam vitae quia neque
+        provident accusamus ad fugit animi beatae tempore rem ut, maxime
+        voluptates impedit.
+      </p>
+      <button class="btn btn-secondary">Learn More</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/header.scss";
+</style>
